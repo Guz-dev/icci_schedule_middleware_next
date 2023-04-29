45cae6c7a51f9dfff4eaafa54232768b26f35f8a
Gustavo <tavo.3900@gmail.com>
2023-04-29 17:11:52 -0400

    Eslint fixes

--- src/pages/api/bloques.js
+++ src/pages/api/bloques.js
@@ -2,14 +2,14 @@ import { promises as fs } from 'fs-extra'
 import { get_data } from '@/services/database.js'
 
 export default function handler(req, res) {
-  console.log("Realiza read");
+  console.log("Realiza read")
   fs.readFile(process.cwd()+'/src/data/data.json', 'utf8')
     .then(data => {
-      console.log("Leyo archivo");
+      console.log("Leyo archivo")
       res.status(200).send(JSON.parse(data))
     })
     .catch(() => {
-      console.log("Realiza fetch");
+      console.log("Realiza fetch")
       get_data()
         .then(data => res.send({ data }))
         .catch((err) => console.log(err))
