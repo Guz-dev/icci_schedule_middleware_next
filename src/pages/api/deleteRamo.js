a1c24e949b951f9f4872092a7730d4939e245ff4
Gustavo <tavo.3900@gmail.com>
2023-05-31 13:48:35 -0400

    Update deleteRamo.js

--- src/pages/api/deleteRamo.js
+++ src/pages/api/deleteRamo.js
@@ -5,7 +5,7 @@ export default async function handler(req, res) {
   await NextCors(req, res, {
     // Options
     methods: ['GET', 'HEAD', 'PUT', 'PATCH', 'POST', 'DELETE'],
-    origin: `https://${process.env.PAGE_ADDRESS}`,
+    origin: `${process.env.PAGE_ADDRESS}`,
     optionsSuccessStatus: 200, // some legacy browsers (IE11, various SmartTVs) choke on 204
   })
 
